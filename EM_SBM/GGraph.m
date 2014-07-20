9bf28072ec74a08c309557c3abf62dbdc2c94e14
Likrain <likrain@users.noreply.github.com>
2014-07-20 17:18:28 +0800

    Rewrite a function to generate graph
    I put the function in the EM_SBM folder. However, it is a function we
    will use globally.

---
+++ EM_SBM/GGraph.m
@@ -0,0 +1,35 @@
+% The function GGraph is generating a directed or undirecte graph 
+% which has community structures via Stochastic Blockmodels.
+% Input: n is the number of nodes.
+%        Pi is the propotion of the different groups.
+%        P is the probability matrix.
+%        gtype is "directed" or "undirected".
+
+% Output: A is the n-by-n adjacent matrix of a graph.
+%         Glabel is the group label of nodes.
+%         Z is the indicator of the group of nodes.
+function [A, Glabel, Z] = GGraph(n, Pi, P, gtype)
+
+A = zeros(n);
+
+Glabel = zeros(1,n);
+
+Z = mnrnd(1, Pi, n);% get indicators
+
+[temp1, temp2] = find(Z);
+
+Glabel(temp1) = temp2; % get group labels
+
+switch gtype
+    case 'directed'
+        A = (Z*P*Z'<=rand(n));
+        A = A - diag(diag(A));
+    case 'undirected'
+        A = (Z*P*Z'<=rand(n));
+        A = triu(A) - diag(diag(A));
+        A = A + A';
+end
+
+
+
+
